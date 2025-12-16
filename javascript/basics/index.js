786251d07d9815a9e773003087c9bfb587583af1
Bhuvan N <bhuvann104@gmail.com>
2025-12-16 09:06:56 +0530

    learn JS data types and operators

--- javascript/basics/index.js
+++ javascript/basics/index.js
@@ -3,8 +3,29 @@ console.log("JavaScript started");
 let name = "FrozenBull";
 const age = "24";
 let isLearning = true;
+let city;
+let score = null;
+let a = 10;
+let b = 3;
+let c = 12;
+
+
 
 
 console.log(name);
 console.log(age);
 console.log(isLearning);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLearning);
+console.log(typeof city);
+console.log(score);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log((a + b) * c);
+
