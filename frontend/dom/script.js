d7d9d44545b7de8b37bf96aa1cd95f5c1adbedbf
Bhuvan N <bhuvann104@gmail.com>
2025-12-18 10:51:01 +0530

    learn DOM basics and cick event

---
+++ frontend/dom/script.js
@@ -0,0 +1,6 @@
+let heading = document.getElementById("title");
+let button = document.getElementById("btn");
+
+button.addEventListener("click", function () {
+  heading.innerText = "Button Clicked!";
+});
